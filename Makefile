b394a7594c1da946af60acfa804969cfc8d8f3e1
royreznik <royreznik@gmail.com>
2022-03-26 05:49:28 -0700

    Adding print function to the real mode

--- Makefile
+++ Makefile
@@ -1,9 +1,2 @@
-run: compile
-	qemu-system-x86_64 dist/boot_sect.bin
-
-compile: dist
-	nasm -f bin boot/boot_sect.asm -o dist/boot_sect.bin
-
-dist:
-	rm -rf dist/
-	mkdir -p dist/
+run:
+	make -C ./boot/ run
--- boot/boot_sect.asm
+++ boot/boot_sect.asm
@@ -1,8 +1,23 @@
 ; Bios loads MBR into 0x7C00, so we want to look from there
 [org 0x7c00]
-    mov bp, 0x8000 ; The stack address
-    mov sp, bp ; if the stack is empty, sp is bp
-    jmp $  ; infinite loop
+
+mov [BOOT_DISK], dl  ; store the current first disk in variable
+mov bx, HELLO
+call print
+mov bp, 0x9000 ; The stack address
+mov sp, bp ; if the stack is empty, sp is bp
+jmp $  ; infinite loop
+
+
+
+%include "boot_print.asm"
+
+
+BOOT_DISK: db 0 ; Just a variable  of the first disk
+
+
+HELLO:
+    db "Hello, World", 0
 
 
 ; $ - the currnet address
